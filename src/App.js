a5634455e0a1ad09e20470f837667cfa420921d6
Frédéric Van Overmeire <frederic.vanovermeire@gmail.com>
2021-11-04 14:40:20 +0100

    task stick around

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
 import uuidv4 from 'uuid/v4'
 
-
+const LOCAL_STORAGE_KEY = "todoApp.todo"
 
 function App() {
   //object destructuring
   const [todo, setTodo] = useState([])
   const addRef = useRef()
+  
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodo(storedTodos)
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todo))
+  },[todo])
 
   function addTodo (e) {
     const addElementTodo = addRef.current.value;
